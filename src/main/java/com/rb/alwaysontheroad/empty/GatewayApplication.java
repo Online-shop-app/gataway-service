cec95a02a74e5de65dcfe2b1216c56636cf06ab1
Rustam Mamedov <rbmamedo@mts.ru>
2022-03-26 20:38:23 +0300

    renamed project

--- src/main/java/com/rb/alwaysontheroad/empty/GatewayApplication.java
+++ src/main/java/com/rb/alwaysontheroad/empty/GatewayApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class EmptyApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmptyApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
--- src/main/java/com/rb/alwaysontheroad/empty/config/SecurityConfig.java
+++ src/main/java/com/rb/alwaysontheroad/empty/config/SecurityConfig.java
@@ -1,24 +1,13 @@
 package com.rb.alwaysontheroad.empty.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import reactor.core.publisher.Mono;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
@@ -30,10 +19,6 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(final ServerHttpSecurity http) {
-        http
-                .oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(grantedAuthoritiesExtractor());
 
         http
                 .authorizeExchange(exchanges -> exchanges
@@ -41,27 +26,13 @@ public class SecurityConfig {
                         .pathMatchers("/actuator/**").permitAll()
                         .pathMatchers(V_3_API_DOCS, SWAGGER_UI, WEBJARS_SWAGGER_UI, SWAGGER_UI_HTML).permitAll()
                         .anyExchange().authenticated()
-                )
-                .csrf().disable();
+                );
 
-        return http.build();
-    }
+        http
+                .oauth2Login(withDefaults());
 
-    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
-        final JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
-        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
-    }
-
-    private static class KeycloakRealmRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
-        @Override
-        public Collection<GrantedAuthority> convert(final Jwt jwt) {
-            final Map<String, Object> claims = jwt.getClaims();
-            final Map<String, Object> realmAccess = (Map<String, Object>) claims.get("realm_access");
-            return ((List<String>) realmAccess.get("roles")).stream()
-                    .map(roleName -> "ROLE_" + roleName)
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toList());
-        }
+        return http
+                .csrf().disable()
+                .build();
     }
 }
