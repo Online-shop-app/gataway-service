57a4f7cb3989537bf4c21f032d13aa03c6bb6298
Rustam Mamedov <alwaysontheroad.rb@gmial.com>
2022-05-30 23:13:14 +0300

    added load-balanced webclient

--- src/main/java/com/rb/alwaysontheroad/gateway/config/HttpClientConfig.java
+++ src/main/java/com/rb/alwaysontheroad/gateway/config/HttpClientConfig.java
@@ -1,8 +1,11 @@
 package com.rb.alwaysontheroad.gateway.config;
 
 import io.netty.handler.logging.LogLevel;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.resources.ConnectionProvider;
 import reactor.netty.transport.logging.AdvancedByteBufFormat;
@@ -10,7 +13,20 @@ import reactor.netty.transport.logging.AdvancedByteBufFormat;
 import java.time.Duration;
 
 @Configuration
-public class AppConfig {
+public class HttpClientConfig {
+
+    @Bean
+    public WebClient webClient(AppProps appProps) {
+        return this.loadBalancedWebClientBuilder()
+                .clientConnector(new ReactorClientHttpConnector(this.httpClient(appProps)))
+                .build();
+    }
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder loadBalancedWebClientBuilder() {
+        return WebClient.builder();
+    }
 
     @Bean
     public HttpClient httpClient(AppProps appProps) {
